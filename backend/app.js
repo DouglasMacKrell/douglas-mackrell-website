fbaa4f76f477d726a44f0720bcadcad2599a9cff
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-06-22 14:26:32 -0400

    heroku start scripts and server details

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 
 module.exports = app;
