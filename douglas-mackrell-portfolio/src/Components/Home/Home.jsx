61f06fb8072bd6d991fd1d06c197e3db225d6b3e
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-03 05:01:12 -0400

    changed target for resume link

--- douglas-mackrell-portfolio/src/Components/Home/Home.jsx
+++ douglas-mackrell-portfolio/src/Components/Home/Home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import YoutubeBackground from "react-youtube-background";
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <YoutubeBackground videoId="KdURSnjeml0" overlay="rgba(0,0,0,.4)" className="home__youtube">
+      <YoutubeBackground
+        videoId="KdURSnjeml0"
+        overlay="rgba(0,0,0,.4)"
+        className="home__youtube"
+      >
         <div className="home__text-container">
           <div className="home__douglas-mackrell">
             <h1>Douglas MacKrell</h1>
           </div>
           <div className="home__resume-button">
             <a
+              target="__blank"
               href="https://docs.google.com/document/d/1oqBG13B7k_H2bzG89-kpQib1bSnEeImcoTGLlQXJKIE/edit?usp=sharing"
               download="Douglas MacKrell Resume"
             >
