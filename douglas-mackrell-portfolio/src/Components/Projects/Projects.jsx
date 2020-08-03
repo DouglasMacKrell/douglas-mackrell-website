b528d885f00d7cec5cbdd1c2eebbf9b2d7851a42
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-02 20:05:47 -0400

    Hand Me Down Card Created

--- douglas-mackrell-portfolio/src/Components/Projects/Projects.jsx
+++ douglas-mackrell-portfolio/src/Components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import ProjectCard from '../ProjectCard/ProjectCard'
 import HandMeDown from './Hand-Me.png'
+import YouDown from './You-Down.png'
 
 import './Projects.css'
 
@@ -21,9 +22,10 @@ const Projects = () => {
             <h1>Projects</h1>
             <ProjectCard
               title="Hand Me Down"
-              content={`Whoa! So cool! Check out this site by clicking <a href="https://handmedown.herokuapp.com/">HERE</a>.`}
+              content={`<em>Hand Me Down</em> is a social media site designed to connect user stories to garments of clothing with the goal of encouraging users to pass down their clothing and their stories to future generations, thereby increasing the value of fast fashion items and decreasing overall waste from fashion consumers. Users can create a Garment in their Digital Wardrobe that gives them the ability to post stories to the History of that Garment. Additionally, they can access a printable iron-on QR Code that links back to the Garment page, so users can pass down their garment and new users can easily connect to its accumulated history!  <hr style="height:2px;color:purple;background-color:purple"> <i class="fas fa-trophy"></i><em>Most Innovative App</em>, Uber + Pursuit 6.0 Hackathon <br> <a href="https://handmedown.herokuapp.com/"><i class="fas fa-link"></i>LIVE LINK</a> <br> <a href="https://github.com/PeterFiorentino/HandMeDown"><i class="fab fa-github"></i>GITHUB REPO</a>`}
               image={HandMeDown}
             />
+            <ProjectCard title="You Down With ELT?" content={`Hello`} image={YouDown} />
           </div>
         </div>
       </div>
